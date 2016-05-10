d35a35a4d88e20565c5415f80615976cc18240d7
BLARG85 <natblarg123@gmail.com>
2016-05-10 13:09:11 -0700

    added ability to create text file with data

--- Lab24b.java
+++ Lab24b.java
@@ -6,7 +6,9 @@
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
+
 import static java.lang.System.*;
 
 public class Lab24b
@@ -16,14 +18,18 @@ public class Lab24b
 		Scanner file = new Scanner(new File ("H:\\Java\\Unit14\\Unit14-Assignments\\lab24b.dat"));
 		int size = file.nextInt();
 		file.nextLine();
+		PrintWriter TestWordSave = new PrintWriter( "H:\\Java\\Unit14\\Unit14-Assignments\\TestSave.txt" );
 		for(int i = 0; i<size; i++)
 		{
 			String word = file.next();
 			System.out.println(word);
+			
+			TestWordSave.write(word+" ");
 
 
 
 
 	   }
+		TestWordSave.close();
 	}
 }
