e39a036dffade7431968bafc9b7f608269fc8c78
BLARG85 <natblarg123@gmail.com>
2016-05-10 23:52:00 -0700

    added reference

--- Encryptor.java
+++ Encryptor.java
@@ -15,14 +15,19 @@ public class Encryptor
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File ("C:\\Users\\majeskia7104\\Desktop\\CAST-128\\CAST-128\\Hello.txt"));
-		//int size = file.nextInt();
-		//file.nextLine();
-		PrintWriter TestWordSave = new PrintWriter( "C:\\Users\\Public\\Pictures\\POOP.txt" );
+		Scanner file = new Scanner(new File ("C:\\Users\\natbl\\Documents\\GitHub\\CAST-128\\Hello.txt"));
+		PrintWriter TestWordSave = new PrintWriter( "C:\\Users\\Public\\Documents\\POOP.txt" );
+		
+		//int keygen = 
+		
 		while(file.hasNext())
 		{
 			String word = file.next();
 			System.out.println(word);
+			
+			for(int i=0; i<word.length(); i++){
+				
+			}
 			
 			TestWordSave.write(word+" ");
 
