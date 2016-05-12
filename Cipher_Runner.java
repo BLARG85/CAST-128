e20390a868b0e7fd8ba6ed208e3d2501e49b49af
BLARG85 <natblarg123@gmail.com>
2016-05-12 13:23:15 -0700

    yfgh

---
+++ Cipher_Runner.java
@@ -0,0 +1,9 @@
+
+public class Cipher_Runner{
+	
+	public static void main(String args[])
+	{
+		//Cast5 run = new Cast5();
+	}
+	
+}
--- Encryptor.java
+++ Encryptor.java
@@ -35,18 +35,36 @@ public class Encryptor
 		TestWordSave.close();
 		*/
 		
-		
-		
 		int binaryInput = 0b101;
+		int int32 = 0b01111111111111111111111111111111;  ///********
+		int int322 = 0b10000000000000000000000000000000;
+		int test = 0b010101;
+		int test1  = 0b11111111111111111111111111101011;
+		short int16 = 0b0101010110010101;
 		// 0b1010; (10)  shifted left once if 5 is times 2
 		// 0b10100; (20) times 2 shifts everything once left
+		byte noterui = 7;
 		int hexInput = 0x32;
 		int input = 5;
 		input <<= 5;
 		
-		System.out.println(binaryInput);
-		System.out.println(hexInput);
-		System.out.println(input);
+		//System.out.println(binaryInput);
+		//System.out.println(hexInput);
+		System.out.println(int32);
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
